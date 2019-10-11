b706c67a2a9b2d8eb6b244d98510988676d87720
igordefo <dekleidung@gmail.com>
2019-10-11 11:01:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var arr = str.split('');
+    var config = bracketsConfig;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < config.length; j++) {
+            if (arr[i] === config[j][0] && arr[i + 1] === config[j][1]) {
+                arr.splice(i, 2);
+                if (arr.length == 0) {
+                    return true;
+                }
+                i = -1;
+            }
+        }
+    }
+    return false;
+};
